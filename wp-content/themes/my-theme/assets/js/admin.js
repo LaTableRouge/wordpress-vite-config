bdf41efdad9720e711503d8f0b151835e43aceb4
MLNOP <victordianamlnop@gmail.com>
2024-11-21 08:50:36 +0100

    feat(theme): updating dependencies & folder/files structure

--- wp-content/themes/my-theme/assets/js/admin.js
+++
@@ -1,5 +0,0 @@
-import '../scss/admin.scss' // mandatory for Hot Module Reload
-
-window.addEventListener('DOMContentLoaded', (e) => {
-    console.log('admin script called')
-})
--- wp-content/themes/my-theme/assets/js/front.js
+++
@@ -1,6 +0,0 @@
-import '../scss/front.scss' // mandatory for Hot Module Reload
-
-window.addEventListener('DOMContentLoaded', (e) => {
-    console.log('front script called')
-    
-})
